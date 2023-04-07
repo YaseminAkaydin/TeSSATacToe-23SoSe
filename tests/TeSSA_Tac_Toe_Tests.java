0c344ebc5d8fb44ed7cb716201df771a214d1e92
acx806 <yasemin.akaydin@haw-hamburg.de>
2023-04-07 12:14:38 +0200

    aufgabe 1.2 beendet, 1.3 angefangen

--- tests/TeSSA_Tac_Toe_Tests.java
+++ tests/TeSSA_Tac_Toe_Tests.java
@@ -1,4 +1,5 @@
 // Version für JUnit 5
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import gfx.MainWindow;
 import gfx.Ressources;
 import logic.Board;
@@ -7,7 +8,8 @@ import logic.WinState;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import javax.swing.JLabel;
+
+import javax.swing.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +17,8 @@ public class TeSSA_Tac_Toe_Tests {
     private Player p1, p2;
     private Board board;
     private MainWindow frame;
+    private MainWindow frame2;
+    private Board board2;
 
     private static final int TIME_OUT = 0;
 
@@ -24,6 +28,8 @@ public class TeSSA_Tac_Toe_Tests {
         p2 = new Player("Player 2", Ressources.icon_o);
         board = new Board(3, 3, 3, p1, p2);
         frame = new MainWindow(p1, p2, board);
+        board2 = new Board(5, 5, 3, p1, p2);
+        frame2= new MainWindow(p1, p2, board2);
         frame.setVisible(true);
         MainWindow.setDebugg(true);
     }
@@ -33,6 +39,8 @@ public class TeSSA_Tac_Toe_Tests {
         p1 = p2 = null;
         board = null;
         frame = null;
+        board2= null;
+        frame2= null;
     }
 
     @Test
@@ -112,11 +120,160 @@ public class TeSSA_Tac_Toe_Tests {
 
     @Test
     public void testSpielstandPlayer1(){
+        JLabel player1Score = frame.getPlayer1_score();
+        JLabel player2Score = frame.getPlayer2_score();
         frame.turn(0, 0);
         frame.turn(0, 1);
         frame.turn(1, 0);
         frame.turn(0, 2);
         WinState winState= frame.turn(2, 0);
+        assertEquals(1, player1Score.getText());
 
     }
+
+    @Test
+    public void testSpielstandPlayer2(){
+        JLabel player1Score = frame.getPlayer1_score();
+        JLabel player2Score = frame.getPlayer2_score();
+
+        frame.turn(0, 1);
+        frame.turn(0, 0);
+        frame.turn(0, 2);
+        frame.turn(1, 0);
+        frame.turn(1, 2);
+        frame.turn(2, 0);
+        frame.checkWinner(WinState.player1);
+        assertEquals(1, player2Score.getText());
+
+    }
+
+    @Test
+    public void testfallNeun(){
+        frame2.turn(0, 0);
+        frame2.turn(0, 1);
+        frame2.turn(1, 0);
+        frame2.turn(1, 1);
+        WinState winState= frame2.turn(3, 0);
+        assertNotEquals(winState, WinState.player1);
+    }
+
+    @Test
+    public void chooseIconTessaRedOrX(){
+        Player player1 = new Player("Player 1", Ressources.icon_o);
+        Player player2 = new Player("Player 2", Ressources.icon_tessa_blue);
+        assertEquals("O", player1.getIconString());
+        assertEquals( "TeSSA blue", player2.getIconString());
+
+    }
+
+    @Test
+    public void mehrfachAnklicken(){
+        String retString= "";
+        String retString2="";
+        for (int i=0; i<20; i++){
+            frame.turn(1,1);
+            retString = board.getPlayerNameInField(1, 1);
+            retString2= board.getPlayerNameInField(0, 0);
+        }
+
+        assertEquals("Player 1", retString );
+        assertEquals("", retString2);
+    }
+
+    @Test
+    public void spielerBautVstruktur(){
+        frame.turn(0, 0);
+        frame.turn(1, 0);
+        frame.turn(1, 1);
+        frame.turn(0, 1);
+        WinState winState= frame.turn(0, 2);
+        assertNotEquals( WinState.player1, winState);
+
+    }
+
+    @Test
+    public void backslashFormation(){
+        frame.turn(0, 0);
+        frame.turn(1, 0);
+        frame.turn(1, 1);
+        frame.turn(0, 1);
+        WinState winState= frame.turn(2, 2);
+        assertEquals(WinState.player1,winState);
+    }
+
+    @Test
+    public void feldWirdNichtAusgewertet(){
+        frame2.turn(4, 4);
+        assertEquals("Player 1", board2.getPlayerNameInField(4, 4));
+    }
+
+    @Test
+    public void testCheckWinnerTie() {
+        JLabel player1Score = frame.getPlayer1_score();
+        JLabel player2Score = frame.getPlayer2_score();
+
+        // Simulate a tie
+        frame.turn(0, 0);
+        frame.turn(1, 0);
+        frame.turn(2, 0);
+        frame.turn(0, 1);
+        frame.turn(1, 1);
+        frame.turn(2, 1);
+        frame.turn(1, 2);
+        frame.turn(2, 2);
+
+        frame.checkWinner(WinState.tie);
+        assertEquals("0", player1Score.getText());
+        assertEquals("0", player2Score.getText());
+
+    }
+
+    @Test
+    public void testCheckWinnerPlayer1(){
+        JLabel player1Score = frame.getPlayer1_score();
+        JLabel player2Score = frame.getPlayer2_score();
+        // Simulate player 1 winning
+        frame.turn(0,0);
+        frame.turn(0,1);
+        frame.turn(1,0);
+        frame.turn(0,2);
+        frame.turn(2,0);
+
+        frame.checkWinner(WinState.player1);
+        assertEquals("1", player1Score.getText());
+        assertEquals("0", player2Score.getText());
+    }
+
+    @Test
+    public void testCheckWinnerPlayer2(){
+        JLabel player1Score = frame.getPlayer1_score();
+        JLabel player2Score = frame.getPlayer2_score();
+
+        // Simulate player 2 winning
+        frame.turn(0,1);
+        frame.turn(0,0);
+        frame.turn(0,2);
+        frame.turn(1,0);
+        frame.turn(2,0);
+        frame.checkWinner(WinState.player2);
+        assertEquals("0", player1Score.getText());
+        assertEquals("1", player2Score.getText());
+    }
+
+    @Test
+    public void testGetIconString() {
+        Player player1 = new Player("X", Ressources.icon_x);
+        Player player2 = new Player("TeSSA red", Ressources.icon_tessa_red);
+        Player player3 = new Player("TeSSA blue", Ressources.icon_tessa_blue);
+        Player player4 = new Player("O", Ressources.icon_o);
+
+        assertEquals("X", player1.getIconString());
+        assertEquals("TeSSA red", player2.getIconString());
+        assertEquals("TeSSA blue", player3.getIconString());
+        assertEquals("O", player4.getIconString());
+    }
+
+
+
+
 }
